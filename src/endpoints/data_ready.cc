b0ffa029c3c0afc3fe879355dacb7f499d0f4fd6
mority <mority@posteo.net>
2025-06-24 11:00:27 +0200

    rm empty line

--- src/endpoints/data_ready.cc
+++ src/endpoints/data_ready.cc
@@ -11,7 +11,6 @@ net::reply data_ready::operator()(net::route_request const& req, bool) const {
 
   auto doc = make_xml_doc();
   auto data_ready_node = doc.append_child("DatenBereitAntwort");
-
   auto ack_node = data_ready_node.append_child("Bestaetigung");
   ack_node.append_attribute("Zst") = timestamp(now()).c_str();
   ack_node.append_attribute("Ergebnis") = "ok";
